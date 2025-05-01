4685baf52ab52816cb2977932e173d914d6a5a67
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-05-01 17:10:29 +0400

    Added banner

--- components/indexBanner/IndexBanner.js
+++ components/indexBanner/IndexBanner.js
@@ -2,7 +2,28 @@ import React from "react";
 import "./indexBanner.css";
 
 const IndexBanner = () => {
-  return <section id="banner">Banner</section>;
+  return (
+    <section id="banner">
+      <div className="container">
+        <div className="banner_container">
+          <div className="banner_left">
+            <h1>Sheepish Serenity Massage</h1>
+            <p>
+              Luxury Wellness & Therapeutic Touch for people of the Working
+              World
+            </p>
+            <small>
+              Serving Orlando, Oviedo, Metrowest, and surrounding Central
+              Florida communities.
+            </small>
+          </div>
+          <div className="banner_right">
+            <img src="/man.png" alt="banner" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default IndexBanner;
